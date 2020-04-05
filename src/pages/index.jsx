d38f0f87d1bbafb689da7976829432a78edaafef
Pavel Fomchenkov <pavel.fomchenkov@gmail.com>
2020-04-05 16:43:30 +0200

    Add Next.js

---
+++ src/pages/index.jsx
@@ -0,0 +1,17 @@
+import Head from 'next/head'
+
+export default () => (
+  <>
+    <Head>
+      <title>Prost!</title>
+    </Head>
+
+    <article>
+      <h1>🍻&thinsp;Prost!</h1>
+      <p>This starter uses:</p>
+      <ul>
+        <li><a href="https://nextjs.org/">Next.js</a></li>
+      </ul>
+    </article>
+  </>
+)
