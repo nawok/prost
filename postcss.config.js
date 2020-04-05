713bca6a162f6be656a10a8a7f505c35f40d7c33
Pavel Fomchenkov <pavel.fomchenkov@gmail.com>
2020-04-05 17:45:26 +0200

    Add Tailwind CSS

---
+++ postcss.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  plugins: {
+    tailwindcss: {},
+    ...(process.env.NODE_ENV === `production` ? {
+      "@fullhuman/postcss-purgecss": {
+        content: [`./src/**/*.jsx`],
+        defaultExtractor: content => content.match(/[\w-/:]+(?<!:)/g) || []
+      },
+      autoprefixer: {}
+    } : {})
+  }
+}
